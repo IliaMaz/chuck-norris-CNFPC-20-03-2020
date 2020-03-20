8fc3da161ebf25fab0bd5dc583f1cd853d4b58e5
Igor MARTY <igor.marty@telecomnancy.net>
2020-03-20 14:01:57 +0100

    update

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -6,8 +6,11 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LanguageService {
   secret = "secret_key=0d1227d3c54c395bcc7bf80a5927fde9"
+
   baseUrl = "https://languagelayer.com/php_helper_scripts/language_api.php?" + this.secret
+
   constructor(private http: HttpClient) { }
+
   getPossiblesLanguages(sentence: string) {
     return this.http.get(this.baseUrl + '&query=' + sentence);
   }
